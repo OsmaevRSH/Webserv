ea9e8b0caf159d516dda2e227c588be28861d01f
Janelle Eldora <jeldora@ox-e5.msk.21-school.ru>
2020-12-17 21:23:49 +0300

    Write function get_page_text

---
+++ get_page_text.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_page_text.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeldora <jeldora@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/17 21:00:58 by jeldora           #+#    #+#             */
+/*   Updated: 2020/12/17 21:15:24 by jeldora          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fstream>
+#include <iostream>
+
+std::string get_page_text(std::string path_to_file)
+{
+	std::ifstream	ifs(path_to_file);
+	std::string		text;
+
+	getline(ifs, text, '\0');
+	ifs.close();
+	return (text);
+}
