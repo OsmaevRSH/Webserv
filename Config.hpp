fb42e94983def157219242e75adf7420e5b95906
jeldora <jeldora@student.21-school.ru>
2020-12-18 21:03:01 +0300

    Started to make class Config, which will parse and save data from config

--- Config.hpp
+++ Config.hpp
@@ -6,12 +6,12 @@
 /*   By: jeldora <jeldora@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/18 19:15:56 by jeldora           #+#    #+#             */
-/*   Updated: 2020/12/18 20:22:23 by jeldora          ###   ########.fr       */
+/*   Updated: 2020/12/18 20:55:26 by jeldora          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "master.hpp"
-
+#include <vector>
 /*
 **	 Значит, в этом классе мы соберем, обработаем и будем хранить всю
 **	информацию из конфигурационного файла.
@@ -25,26 +25,32 @@
 **
 */
 
-typedef struct	s_location
+typedef struct					s_location
 {
-	std::string	root;
-}				t_location;
+	std::string					root;
+	std::vector<std::string>	allow_methods;
+}								t_location;
 
-typedef struct	s_server
+typedef struct					s_server
 {
-	std::string	host;			// Тоже самое что и listen в конфиге. Порт и ip разделены двоеточием.
-	t_location	*locations;
+	std::string					listen;			// Тоже самое что и listen в конфиге. Порт и ip разделены двоеточием.
+	std::vector<t_location>		locations;
+	std::string					server_name;	// Сравниваем с host заголовком в реквесте.
 	// Два отдельных поля, чтобы было удобнее обращаться к ip и порту.
-	std::string ip;
-	int			port;
-}				t_server;
+	std::string 				ip;
+	int							port;
+}								t_server;
 
 class Config
 {
 	private:
-		std::string	_config_text;
-		t_server	*_servers;
-		t_server	_default_server;
+		std::string				_config_text;	// Сырой текст конфига
+		std::vector<t_server>	_servers;		
+		t_server				_def_server;
+		std::string				_def_err_page;
+		std::string 			_request_is_dir; // Файл, который будет возвращаться, если реквест есть директория
+		size_t					_limit_client_body_size;
+		bool					_autoindex;
 	public:
 		Config(const std::string& path_to_config);
 };
