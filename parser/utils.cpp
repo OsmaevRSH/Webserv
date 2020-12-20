251e86b5f44e95039af10f766df730a07d3185d1
Janelle Eldora <jeldora@vo-j5.msk.21-school.ru>
2020-12-20 20:25:53 +0300

    process'

--- parser/utils.cpp
+++ parser/utils.cpp
@@ -6,7 +6,7 @@
 /*   By: jeldora <jeldora@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/20 17:53:16 by jeldora           #+#    #+#             */
-/*   Updated: 2020/12/20 18:24:10 by jeldora          ###   ########.fr       */
+/*   Updated: 2020/12/20 20:21:35 by jeldora          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ bool				next_word_pos(const std::string& str, size_t &pos)
 	return true;
 }
 
-size_t			word_len(const std::string& str, const size_t &pos)
+size_t				word_len(const std::string& str, const size_t &pos)
 {
 	size_t length = 0;
 
@@ -46,4 +46,47 @@ size_t			word_len(const std::string& str, const size_t &pos)
 											str[pos + length] != '\n'))
 		length++;
 	return length;
+}
+
+static size_t		block_directive(const std::string &text, const size_t &pos)
+{
+	size_t pos_1 = text.find('{', pos);
+	size_t pos_2 = pos_1;
+	size_t tmp_pos;
+
+	tmp_pos = pos_2;
+	while ((pos_2 = text.find('}', pos_2)) != std::string::npos)
+	{
+		tmp_pos = text.rfind('{', --tmp_pos);
+		if (tmp_pos == pos_1)
+			return pos_2;
+		else if (tmp_pos < pos_1)
+			show_error(text, pos_2);
+	}
+	show_error(text, pos_2);
+	return std::string::npos;
+}
+
+static size_t		simple_directive(const std::string &text, const size_t &pos)
+{
+	size_t pos_end;
+
+	pos_end = text.find(';', pos);
+	if (pos_end == std::string::npos)
+		show_error(text, pos);
+	return pos_end;
+}
+
+size_t				end_directive(const std::string &text, const size_t &pos)
+{
+	size_t simple = 0;
+	size_t block = 0;
+	simple = text.find(';', pos);
+	block = text.find('{', pos);
+	if (simple == block)
+		show_error(text, pos);
+	if (simple == std::string::npos || simple > block)
+		return block_directive(text, pos);
+	else
+		return simple_directive(text, pos);
 }
