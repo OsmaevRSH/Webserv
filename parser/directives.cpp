6b9a68cf81f0e9630f0790b7c01fc3083004c021
Janelle Eldora <jeldora@vo-j5.msk.21-school.ru>
2020-12-20 18:30:12 +0300

    Parser in process

---
+++ parser/directives.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   directives.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeldora <jeldora@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/20 17:14:50 by jeldora           #+#    #+#             */
+/*   Updated: 2020/12/20 18:25:28 by jeldora          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "master.hpp"
+
+void				parse_server()
+{
+	std::vector<std::string> server_context;
+
+	server_context.push_back("index");
+	server_context.push_back("max_body_size");
+	server_context.push_back("root");
+	server_context.push_back("autoindex");
+	server_context.push_back("ip");
+	server_context.push_back("port");
+	server_context.push_back("server_name");
+	server_context.push_back("route");
+
+}
+
+void				parse_route()
+{
+	std::vector<std::string> route_context;
+
+	route_context.push_back("index");
+	route_context.push_back("max_body_size");
+	route_context.push_back("root");
+	route_context.push_back("autoindex");
+	route_context.push_back("allow");
+	route_context.push_back("route");
+}
+
+void				block_directive()
+{
+	
+}
+
+void				parse_index()
+{
+
+}
