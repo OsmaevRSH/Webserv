4c46733c106623c41280e486c5f5feabf29f7215
ltheresi <osmaev.ruslan.bmstu@gmail.com>
2021-02-07 20:40:02 +0300

    add start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
-mkdir -p Eval && cd Eval/ && cmake ../ && make && ./Webserv
+if [ -e "which cmake" ]; then
+      echo "start Webserv";
+      mkdir -p Eval && cd Eval/ && cmake ../ > /dev/null && make > /dev/null && ./Webserv;
+else
+      if [[ $OS == "OSX" ]]; then
+          echo "brew install";
+          @brew install cmake;
+      fi
+      echo "start Webserv";
+      mkdir -p Eval && cd Eval/ && cmake ../ > /dev/null && make > /dev/null && ./Webserv;
+fi
