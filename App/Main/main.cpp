afd634f6ab50121151e280dbf214a2c5a438e705
ltheresi <osmaev.ruslan.bmstu@gmail.com>
2021-02-13 18:29:33 +0300

    update main

--- App/Main/main.cpp
+++ App/Main/main.cpp
@@ -4,19 +4,13 @@
 
 int main(int argc, char **argv, char **env)
 {
-	MIME_ERROR mime;
-#ifndef TESTER
-	Parser config("./App/Config/conf");
-#endif
-#ifdef TESTER
 	if (argc != 2)
 	{
 		std::cout << "Error: bad number of argument\n";
 		exit(1);
 	}
-//	Parser config(argv[1]);
-	Parser config("./App/Config/conf_for_tester");
-#endif
+	MIME_ERROR mime;
+	Parser config(argv[1]);
 	Server server(config.getServers(), config.getErrorPages(), config.getEw(), mime,env);
 	server.server_start();
 	return 0;
